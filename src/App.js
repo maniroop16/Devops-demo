c6bf0fc0ada51c4e9b3f211d7f788ca0192171fe
SaiManiroopM <saimaniroopm@gmail.com>
2021-11-03 17:42:26 +0530

    Updated title

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let page = "This is smallapp";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <b>{page}</b>
         <p>
           For GIT Session
         </p>
